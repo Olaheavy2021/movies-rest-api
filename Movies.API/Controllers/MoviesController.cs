59a9c5aab7b59496bcafdd4538be97a644c5aa3c
Sunkanmi Olawuwo <sunkanmiolawuwo.gas@gmail.com>
2025-05-31 19:38:12 +0100

    feat: Implement the controllers and repository

---
+++ Movies.API/Controllers/MoviesController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.API.Mapping;
+using Movies.Application.Repositories;
+using Movies.Contracts.Requests;
+
+namespace Movies.API.Controllers;
+
+[ApiController]
+public class MoviesController(IMovieRepository movieRepository) : ControllerBase
+{
+    [HttpPost(ApiEndpoints.Movies.Create)]
+    public async Task<IActionResult> CreateAsync([FromBody] CreateMovieRequest request)
+    {
+        var movie = request.MapToMovie();
+
+        await movieRepository.CreateAsync(movie);
+        return CreatedAtAction(nameof(GetAsync), new { id = movie.Id }, movie.MapToResponse());
+    }
+
+    [HttpGet(ApiEndpoints.Movies.Get)]
+    public async Task<IActionResult> GetAsync([FromRoute]Guid id)
+    {
+        var movie = await movieRepository.GetByIdAsync(id);
+        if (movie is null)
+        {
+            return NotFound();
+        }
+        return Ok(movie.MapToResponse());
+    }
+
+    [HttpGet(ApiEndpoints.Movies.GetAll)]
+    public async Task<IActionResult> GetAllAsync()
+    {
+        var movies = await movieRepository.GetAllAsync();
+        return Ok(movies.MapToResponse());
+    }
+
+    [HttpPut(ApiEndpoints.Movies.Update)]
+    public async Task<IActionResult> UpdateAsync([FromRoute] Guid id, [FromBody] UpdateMovieRequest request)
+    {
+       var movie = request.MapToMovie(id);
+        var updated = await movieRepository.UpdateAsync(movie);
+        if (!updated)
+        {
+            return NotFound();
+        }
+       var response = movie.MapToResponse();
+        return Ok(response);
+    }
+
+    [HttpDelete(ApiEndpoints.Movies.Delete)]
+    public async Task<IActionResult> DeleteAsync([FromRoute] Guid id)
+    {
+        var deleted = await movieRepository.DeleteByIdAsync(id);
+        if (!deleted)
+        {
+            return NotFound();
+        }
+        return NoContent();
+    }
+}
---
+++ Movies.Contracts/Responses/MovieResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Movies.Contracts.Responses;
+
+public class MovieResponse
+{
+    public required Guid Id { get; init; }
+    public required string Title { get; init; }
+
+    public required int YearOfRelease { get; init; }
+
+    public IEnumerable<string> Genres { get; init; } = Enumerable.Empty<string>();
+}
